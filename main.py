d77f6eaeb1371c3cdaf8489e8665ca0668db6e07
Evan Wright <emw8105@gmail.com>
2025-11-24 02:59:25 -0600

    dataset loading and grades added

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
+from flask import json
 import numpy as np
+import os
+import glob
 import matplotlib.pyplot as plt
 import seaborn as sns
 import pandas as pd
@@ -13,3 +16,51 @@ from sklearn.multioutput import MultiOutputRegressor
 from sklearn.svm import SVR
 from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
 
+# load the professor data
+def load_professor_dataset():
+    professor_path = os.path.join("data", "matched_professor_data.json")
+    try:
+        with open(professor_path, "r", encoding="utf-8") as file:
+            print(f"Loading professor dataset {professor_path}")
+            prof_dict = json.load(file)
+    except Exception as e:
+        print(f"Error loading professor dataset {professor_path}: {e}")
+        raise
+
+    # instructor_id is the key from the json dataset, i.e. jxc064000
+    df = pd.DataFrame.from_dict(prof_dict, orient="index")
+    df.index.name = "instructor_id"
+    df.reset_index(inplace=True)
+
+    # TF-IDF will need space-separated strings so we aggregate tags list into single string, and smooth out any formatting inconsistencies
+    df["tags_text"] = df["tags"].apply(lambda x: " ".join(x) if isinstance(x, list) else "")
+    df["instructor_id"] = df["instructor_id"].astype(str).str.strip()
+
+    return df
+
+# load the grade data, used for extra insights (aggregated values use placeholders for withdrawal for example)
+def load_all_grade_data():
+    all_grades = []
+
+    # iterate over the grades files in /data
+    for filename in os.listdir("data"):
+        if filename.startswith("enhanced_grades_") and filename.endswith(".csv"):
+            grades_path = os.path.join("data", filename)
+            print(f"Loading grades file {grades_path}")
+            df = pd.read_csv(grades_path)
+            all_grades.append(df)
+
+    if not all_grades:
+        raise ValueError("No enhanced_grades_*.csv files found in /data directory.")
+
+    # aggregate all grades into one df and fixing silent failed merges due to any formatting inconsistencies
+    grades_df = pd.concat(all_grades, ignore_index=True)
+    grades_df["instructor_id"] = grades_df["instructor_id"].astype(str).str.strip()
+
+    return grades_df
+
+prof_df = load_professor_dataset()
+grades_df = load_all_grade_data()
+
+# complete record of grade dist per course per semester, RMP ratings, agg grade ratings, tags, difficulty, would take again %, etc.
+merged_df = grades_df.merge(prof_df, on="instructor_id", how="left")
